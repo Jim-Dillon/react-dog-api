4db897f8715f3c389be6ec2862892192f6a8b1c2
Jim-Dillon <james.dillon94@gmail.com>
2024-03-14 11:36:44 +0000

    add error state for fetch request fail

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ const App = () => {
   const [breeds, setBreeds] = useState([]);
   const [selectedBreed, setSelectedBreed] = useState('');
   const [dogImage, setDogImage] = useState('');
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch breeds');
+        }
+        return response.json();
+      })
       .then(data => {
         const breedsArray = Object.keys(data.message);
         setBreeds(breedsArray);
       })
-      .catch(error => console.error('Error fetching breeds:', error));
+      .catch(error => {
+        setError(error.message);
+        console.error('Error fetching breeds:', error);
+      });
   }, []);
 
   const handleBreedChange = async (event) => {
@@ -24,9 +33,13 @@ const App = () => {
 
     try {
       const response = await fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch dog image');
+      }
       const data = await response.json();
       setDogImage(data.message);
     } catch (error) {
+      setError(error.message);
       console.error('Error fetching dog image:', error);
     }
   };
@@ -40,7 +53,11 @@ const App = () => {
           <p className='disclaimer'>Photos courtesy of <a href='https://dog.ceo/dog-api/'>Dog Ceo API</a>.</p>
           <Dropdown breeds={breeds} selectedBreed={selectedBreed} onBreedChange={handleBreedChange} />
         </div>
-        <DogImage imageUrl={dogImage} />
+        {error ? (
+          <div className="error-message">Sorry, we can't do that right now.</div>
+        ) : (
+          <DogImage imageUrl={dogImage} />
+        )}
       </div>
     </div>
   );
